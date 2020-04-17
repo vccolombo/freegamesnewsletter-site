32fa71675f1ec9751e5e8352cc136e6a539f61f9
Victor Colombo <victorcora98@gmail.com>
2020-04-17 18:08:03 -0300

    Add recaptcha to subscription

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const axios = require('axios');
 const { check, body, validationResult } = require('express-validator');
 
 const subscriptionManager = require('./subscriptionManager');
+
+require('dotenv').config();
+const RECAPTCHA_KEY = process.env.RECAPTCHA_KEY;
+const RECAPTCHA_URL = 
+    'https://www.google.com/recaptcha/api/siteverify?secret=' + RECAPTCHA_KEY;
 
 const router = express.Router();
 
@@ -14,18 +20,29 @@ router.post('/subscribe', [
 ], (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+        console.log(errors);
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const email = req.body.email;
-    subscriptionManager.subscribe(email, (err, success) => {
-        return res.redirect('/subscribe-success');
-    });
+    const verificationUrl = RECAPTCHA_URL 
+        + "&response=" + req.body['g-recaptcha-response'] 
+        + "&remoteip=" + req.connection.remoteAddress;
+    axios.get(verificationUrl).then((response) => {
+        if (response.data.success) {
+            const email = req.body.email;
+            subscriptionManager.subscribe(email, (err, success) => {
+                return res.redirect('/subscribe-success');
+            });
+        } else {
+            const errors = { 'errors': response.data['error-codes'] };
+            return res.status(400).json(errors);
+        }
+    }); 
 });
 
 router.get('/subscribe-success', (req, res) => {
     res.render('pages/subscribeSuccess');
-})
+});
 
 router.get('/unsubscribe', [
     check('email').unescape().isEmail().normalizeEmail(),
@@ -41,6 +58,8 @@ router.get('/unsubscribe', [
         console.log(errors);
         return res.render('pages/unsubscribeFailed');
     }
+
+    axios.get()
 
     const email = req.query.email;
     const code = req.query.code;
--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
