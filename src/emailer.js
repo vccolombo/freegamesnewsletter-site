d6b08f332f9192344f5fe25afaa2a00fbc2454fc
Víctor Colombo <victorcora98@gmail.com>
2020-07-16 18:34:59 -0300

    Use mailer microservice to send confirmation email (#1)
    Confirmation emails are now sent to the mailer microservice via RabbitMQ

--- src/emailer.js
+++ src/emailer.js
@@ -1,15 +1,10 @@
-const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 
 const hash = require('./hash').hash;
 const db = require('./db');
+const broker = require('./broker');
 
-const SMTP_HOST = 'smtp.gmail.com';
-const SMTP_PORT = 465;
-
-const SENDER_EMAIL = 'freegamesnewsletter@gmail.com';
-const SENDER_PASSWORD = process.env.EMAIL_PASS;
-
+const SITE_URL = process.env.SITE_URL;
 const SUBJECT = 'Confirm your subscription';
 
 async function sendConfirmationEmail(email) {
@@ -20,42 +15,19 @@ async function sendConfirmationEmail(email) {
 
     console.log('Sending confirmation email to:' + email);
 
-    let transporter = createTransport();
-    transporter.verify(function(error, success) {
-        if (error) {
-            return console.log(error);
-        }
+    generateHTMLmessage(email).then((html) => {
+        msg = {
+            'email': email,
+            'subject': SUBJECT,
+            'html': html
+        };
 
-        console.log('Ready to send messages via SMTP');
-            
-        generateHTMLmessage(email)
-            .then((html) => {
-                transporter.sendMail({
-                    from: SENDER_EMAIL,
-                    to: email,
-                    subject: SUBJECT,
-                    html: html,
-                }, (err, info) => {
-                    console.log('Confirmation email sent to %s: %s', email, info);
-                });
-            });
-    });
-};
-
-function createTransport() {
-    return nodemailer.createTransport({
-        host: SMTP_HOST,
-        port: SMTP_PORT,
-        secure: true,
-        auth: {
-            user: SENDER_EMAIL,
-            pass: SENDER_PASSWORD,
-        },
+        broker.publish('', 'emails', JSON.stringify(msg));
     });
 };
 
 async function generateHTMLmessage(email) {
-    const confirmUrl = 'https://www.freegamesnewsletter.tech/confirmEmail?email=' + email + '&code=' + hash(email);
+    const confirmUrl = SITE_URL + '/confirmEmail?email=' + email + '&code=' + hash(email);
 
     return await ejs.renderFile('src/email_templates/confirmation.html', {confirmUrl}, {async: true});
 };
